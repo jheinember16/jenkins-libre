ee9fc68057aec6720befc75dbad651667331eb38
Jheinember <jheinemberstithjn@ufps.edu.co>
2025-07-05 02:06:44 -0500

    Nuevos Cambios test

--- Proyecto01/api-rest-publicaciones/src/test/java/com/api/rest/publicaciones/PublicacionApplicationTests.java
+++ Proyecto01/api-rest-publicaciones/src/test/java/com/api/rest/publicaciones/PublicacionApplicationTests.java
@@ -1,5 +1,6 @@
 package com.api.rest.publicaciones;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PublicacionApplicationTests {
 
 	@Test
-        @Disabled  
+	@Disabled
 	void contextLoads() {
 	}
 
